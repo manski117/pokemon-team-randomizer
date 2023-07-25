d73c84d2fc4543fb510dbb086534118bae00b9c9
krmanski <choromanski117@gmail.com>
2023-07-25 11:45:26 -0400

    Implement footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,15 +22,6 @@ import {
 //components
 import TeamSlot from "./components/TeamSlot";
 
-//TODO TOMORROW:
-/*
-Main issue X: My edits in Team Slot components work just fine and are saved on the tempObj in their state.
-The problem is that I'm trying to get those changes to show up on my export
-Way I'm trying to solve this issueY: setState is asynch so I can only update the team state once the 
-data from ALL components has been passed to this one. I'm currently putting them in a little container that 
-is called stagedTeam. The main problem is that for some reason, even though stagedTeam is being changed,
-those changes are somehow gone when they get to the updateTeam call.
-*/
 
 const Home: NextPage = () => {
   const [teamData, setTeamData] = React.useState<string>("");
@@ -563,30 +554,86 @@ const Home: NextPage = () => {
                   exportFinal={sendNewDataToRoot}
                 />
               </div>
-              <label htmlFor="my-modal" className="btn w-[360px] md:w-[750px] lg:w-full" onClick={exportData}>
+              <label htmlFor="my-modal" className="btn w-[360px] md:w-[750px] lg:w-full my-3" onClick={exportData}>
                 Export Team
               </label>
             </div>
           </section>
-          <div id="bottom-button" className="w-full mx-auto">
-            export button HERE
-          </div>
+
         </div>
 
-        <footer className="footer p-10 bg-neutral text-neutral-content">
-          <div>
-            <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clip-rule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-            <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
-          </div> 
-          <div>
-            <span className="footer-title">Social</span> 
-            <div className="grid grid-flow-col gap-4">
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+          <div className="items-center flex flex-row">
+            <a className="w-[150px] flex justify-center" href="#">
+              <img
+                className="object-fit w-[150px]"
+                src="/poketooldrop_logo_temporary.png"
+                alt="logo"
+                title="Building tools for the pokemon competitive community"
+              />
+            </a>
+
+            <div id="footer-disclaimer" className=" flex flex-col justify-center">
+              <h4 className="hidden md:block text-base font-medium">
+                Tool Disclaimer:
+              </h4>
+              <p>Tool is free and not for profit.</p>
+              <p className="hidden md:block">
+                Randomizer designed for use with
+                <a href="https://pokemonshowdown.com/">Pokemon Showdown.</a>
+              </p>
+              <p className="hidden md:block">
+                Tool designed for team generation use by the pokemon community.
+              </p>
             </div>
+
+            <div id="footer-credits" className=" flex flex-col justify-center">
+              <h4 className="hidden md:block text-base font-medium">Tool Credits:</h4>
+              <p>Developed by @manski117.</p>
+              <p className="hidden md:block">
+                Data courtesy of <a href="https://github.com/smogon/data">Smogon</a>.
+              </p>
+              <p className="hidden md:block">
+                Images courtesy of{" "}
+                <a href="https://www.pngmart.com/image/391885" target="_blank">
+                  pngmart,
+                </a>{" "}
+                <a href="https://pokeapi.co/">pokeapi.co</a>, and{" "}
+                <a href="https://icons8.com/">icons8.com</a>.{" "}
+              </p>
+            </div>
+
+            <div id="social-icons" className="flex flex-row w-fit gap-3 h-fit flex justify-center">
+            <a href="https://github.com/manski117/pokemon-team-randomizer">
+              <img
+                src="https://img.icons8.com/ios-filled/50/c2cbf5/github.png"
+                className="w-6 h-6"
+                alt="link to author's github"
+                title="link to author's github"
+              />
+            </a>
+            <a href="https://github.com/manski117">
+              <img
+                src="https://img.icons8.com/ios-filled/50/c2cbf5/portfolio.png"
+                className="w-6 h-6"
+                alt="link to author's portfolio website"
+                title="link to author's portfolio website"
+              />
+            </a>
+            <a href="#">
+              <img
+                src="https://img.icons8.com/material-rounded/24/c2cbf5/home.png"
+                className="w-6 h-6"
+                alt="link to homepage"
+              />
+            </a>
+            
           </div>
+          </div>
+
+
         </footer>
+
 
 
         
